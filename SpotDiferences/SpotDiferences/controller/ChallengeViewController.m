b683bb981cdf9b5fdd7dd6df91afebb086b49dac
greent911 <greent911@gmail.com>
2013-06-10 23:39:21 +0800

    feature7 init

--- SpotDiferences/SpotDiferences/controller/ChallengeViewController.m
+++ SpotDiferences/SpotDiferences/controller/ChallengeViewController.m
@@ -80,6 +80,10 @@
     
     [self.CH1 addTarget:self action:@selector(CH1ButtonTapped:) forControlEvents:UIControlEventTouchUpInside];
     
+    UIButton *bbutton = [UikitFramework createButtonWithBackgroudImage:@"backButton" title:@"" positionX:10 positionY:10];
+    bbutton.frame = CGRectMake(0, 205, 60, 40);
+    [bbutton addTarget:self action:@selector(backButtonTapped:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:bbutton];
     
     int level = [prefs integerForKey:@"level"];
     
@@ -102,6 +106,12 @@
     
     //[self.view addSubview:ChallengeButton];
 
+}
+
+-(void)backButtonTapped:(UIButton*)sender
+{
+    [self playInterfaceSound];
+    [[self navigationController]popViewControllerAnimated:YES];
 }
 
 -(void) CH1ButtonTapped:(UIButton*)sender
@@ -142,7 +152,7 @@
     [prefs synchronize];
     //NSArray *allMAzes = [Maze getMazeByState:@"normal" inManagedObjectContext:self.document.managedObjectContext];
     
-    NSArray *allMAzes = [Maze getMazeByState:@"normal" inManagedObjectContext:self.document.managedObjectContext];
+    NSArray *allMAzes = [Maze getMazeByState:@"challenge" inManagedObjectContext:self.document.managedObjectContext];
     
 //    Maze *maze=[Maze getMazeByName:@"birds" inManagedObjectContext:self.document.managedObjectContext];
 //    NSLog(maze.name);
